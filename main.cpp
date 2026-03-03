1a43fd5f8881af2007998bc5b1393a78dcfaa4fd
Lucas Pujol <lucas.pujol@epitech.eu>
2026-03-03 15:20:17 +0100

    feat(main): add main function for application entry point and argument parsing

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include "App.hpp"
+#include <iostream>
+
+int main(int argc, char **argv) {
+    clap::App app(argv[0], "my super program");
+    
+    auto &verbose = app.flag("-v,--verbose", "Enable verbose output");
+    auto &count = 
+        app.option<int>("-c,--count", "Number of times to do something")
+        .required();
+        // .default_value(1);
+    app.parse(argc, argv);
+    
+    if (verbose)
+        std::cout << "Verbose mode enabled\n";
+
+    std::cout << "Count: " << count.get() << "\n";
+
+    return 0;
+}
