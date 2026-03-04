e19d26f7032b4be97c3a772e9fc7b7d21d592579
Lucas Pujol <lucas.pujol@epitech.eu>
2026-03-04 10:40:55 +0100

    feat(ParseValue): add template struct for parsing values from strings

---
+++ src/ParseValue.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <string>
+
+#include "ClapExceptions.hpp"
+
+namespace clap {
+    template<typename T>
+    struct ParseValue {
+        static T parse(std::string_view str) {
+            std::istringstream iss{std::string(str)};
+            T val;
+            if (!(iss >> val))
+                throw clap::ParseError("Failed to parse value");
+            return val;
+        }
+    };
+}
