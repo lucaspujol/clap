e39fd9a9837d84751915b8e11d3b22857a3be36b
Lucas Pujol <lucas.pujol@epitech.eu>
2026-07-15 19:41:18 +0200

    refactor: move output statements after exception handling in zappy_server example

--- examples/zappy_server.cpp
+++ examples/zappy_server.cpp
@@ -12,24 +12,26 @@ int main(int argc, char **argv) {
     auto &freq = app.option<int>("-f,--freq", "frequency of the server (events/s)").default_value(100);
     auto &seed = app.option<int>("-s,--seed", "RNG seed for reproducible games").default_value(8787);       // shoulda be random but i cant be asked
 
-    
+
     try {
         app.parse(argc, argv);
-        std::cout << "port:    " << port.get() << std::endl;
-        std::cout << "width:   " << width.get() << std::endl;
-        std::cout << "height:  " << height.get() << std::endl;
-        std::cout << "teams:   ";
-        for (const auto &team : teams.get())
-            std::cout << team << " ";
-        std::cout << std::endl;
-        std::cout << "clients: " << clients.get() << std::endl;
-        std::cout << "freq:    " << freq.get() << std::endl;
-        std::cout << "seed:    " << seed.get() << std::endl;
     }
     catch (const clap::HelpRequested&) { return 0; }
     catch (const clap::ClapException &e) {
         std::cerr << "Error: " << e.what() << "\n" << app.usage() << std::endl;
         return 84;
     }
+
+    std::cout << "port:    " << port.get() << std::endl;
+    std::cout << "width:   " << width.get() << std::endl;
+    std::cout << "height:  " << height.get() << std::endl;
+    std::cout << "teams:   ";
+    for (const auto &team : teams.get())
+        std::cout << team << " ";
+    std::cout << std::endl;
+    std::cout << "clients: " << clients.get() << std::endl;
+    std::cout << "freq:    " << freq.get() << std::endl;
+    std::cout << "seed:    " << seed.get() << std::endl;
+
     return 0;
 }
