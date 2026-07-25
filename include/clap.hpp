6c766015448fbe40f0104cef086d7c8ccfa85975
Lucas Pujol <145355928+lucaspujol@users.noreply.github.com>
2026-07-25 22:59:45 +0200

    feat(Flag): new .count() to get how many time a flag has been set (#61)
    Very straight forward pr:
    
    * added `_count` private member variable to Flag concrete class
    * when parsing a flag, its `_value` is set to true and its `_count` is incremented
    * added new `.count()` public member method that returns the internal `_count
    
    > NOTE: this pr doesnt ship any tests because we will refactor how tests are compiled, allowing to see branches. Because most of the lib is inline and/or templated, the gcovr only sees around 50-60% of the actual lib code. Next PR will change that to reflect the real coverage values, and ship the necessary tests. Expect tests to be split in files and organized differently

--- include/clap.hpp
+++ include/clap.hpp
@@ -490,6 +490,8 @@ namespace clap {
 // ===== Flag.hpp =====
 namespace clap {
     /// A boolean switch like -v or --force. Convert to bool to read it.
+    /// You can also call .count() to see how many times it was repeated.
+    /// example: -vvv sets count() to 3 (if you want a level of verboseness).
     class Flag : public Argument {
         public:
             Flag(std::string names, std::string description)
@@ -497,16 +499,23 @@ namespace clap {
 
             operator bool() const noexcept { return _value; }
 
-            void parse(std::string_view, bool discard) override {
-                if (!discard) _value = true;
+            void parse(std::string_view, bool discard) noexcept override {
+                if (!discard) {
+                    _value = true;
+                    _count++;
+                }
             }
+
             std::string_view type_name() const override { return ""; }
 
             bool is_set() const noexcept override { return _value; }
             bool takes_value() const noexcept override { return false; }
 
+            int count() const noexcept { return _count; }
+
         private:
             bool _value = false;
+            int _count = 0;
     };
 }
 
